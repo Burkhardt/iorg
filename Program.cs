f216fcd2003ded14a0b2ea24bb7cd01e392f2221
Rainer Burkhardt <Rainer@Burkhardt.com>
2019-09-29 15:30:50 -0700

    cli main programm - fetches parameter from the commandline; call w/o param for help

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Image.Base;
+using OperatingSystemCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace iorg
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region check for proper calling
+            if (args.Length < 1 || args.Length > 4)
+            {
+                Console.WriteLine($"usage: [filter=<filter>] [remove=<removeString>] [from=<sourceDirectory>] [to=<destDirectory>]");
+                Console.WriteLine("examples:");
+                Console.WriteLine("\tiorg filter=*.png from=. to=.");
+                Console.WriteLine("\tiorg filter=*.jpeg from=. to=..");
+                return;
+            }
+            #endregion
+            #region get all options, named and unnamed params
+            var options = from _ in args where _[0] == '-' select _;
+            var namedVars = from _ in args where _.Contains("=") select _;
+            var unnamedVar = from _ in args where !(_[0] == '-' || _.Contains("=")) select _;
+            #endregion
+            var p = new Dictionary<string, string>();
+            #region set named parameters to dictionary
+            foreach (var elem in namedVars)
+            {
+                var list = elem.Split('=');
+                p.Add(list[0], list[1]);
+            }
+            #endregion
+            #region set all params for the rename call
+            if (!p.ContainsKey("to") || string.IsNullOrEmpty(p["to"]) || p["to"] == ".")
+                p["to"] = Directory.GetCurrentDirectory();
+            if (!p.ContainsKey("from") || string.IsNullOrEmpty(p["from"]) || p["from"] == ".")
+                p["from"] = Directory.GetCurrentDirectory();
+            if (!p.ContainsKey("filter") || string.IsNullOrEmpty(p["filter"]))
+                p["filter"] = "*.jpg";
+            if (!p.ContainsKey("remove") || string.IsNullOrEmpty(p["remove"]))
+                p["remove"] = "";
+            #endregion
+            var count = ImageTreeFile.MoveToTree(
+                    fromDir: p["from"], 
+                    toDirRoot: p["to"], 
+                    filter: p["filter"], 
+                    remove: p["remove"]); 
+            Console.WriteLine($"{count} files moved.");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # iorg
 Reorganize images from a source directory into a directory tree - cli project
+
+## Compile for Standalone
+
+    dotnet publish -r win-x64 -c release
+
+_OR_
+
+    dotnet publish -r osx.10.14-x64 -c release 
+
+The cli call prints out wher to find the resulting binary - in my case: 
+
+<samp><small>`iorg -> ~/ProjectsCore/imgorg/iorg/bin/Release/netcoreapp3.0/osx.10.14-x64/publish/`</small></samp>
+
+And the self contained binary for macOS I find there is named `iorg` (~70MB in size):
+
+<small>`ls -la ~/ProjectsCore/imgorg/iorg/bin/Release/netcoreapp3.0/osx.10.14-x64/publish/`</small>
+|  |  |  |  |  |  |  | 
+ | --- | --- | --- | --- | ---: | --- | --- |
+drwxr-xr-x | 4   | RSB | staff |      128 | Sep 29 14:47 | . |
+drwxr-xr-x | 254 | RSB | staff |     8128 | Sep 29 14:46 | .. |
+-rwxr-xr-x |   1 | RSB | staff | 73927287 | Sep 29 14:47 | **iorg** |
+-rw-r--r-- |   1 | RSB | staff |      812 | Sep 29 14:46 | iorg.pdb |
+
+For a more detailed description see [ImgOrg.Tests](https://github.com/Burkhardt/ImgOrg.Test).
